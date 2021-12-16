fbfb471bb6bbfc4cd00849058ce9849f8d1b547b
Han Lim <sc19hhl@leeds.ac.uk.com>
2021-12-16 10:37:26 +0000

    Code cleaning and comments

--- camera.cpp
+++ camera.cpp
@@ -1,38 +1,58 @@
 #include "camera.h"
 
 Camera::Camera(glm::vec3 position) {
+  // Camera properties
   _position = position;
-  _front = glm::vec3(0.0f, 0.0f, -1.0f);
-  _worldUp = glm::vec3(0.0f, 1.0f, 0.0f);
+  _front    = glm::vec3(0.0f, 0.0f, -1.0f);
+  _worldUp  = glm::vec3(0.0f, 1.0f, 0.0f);
 
-  _yaw = -90.0f;
+  // Euler angles
+  _yaw   = -90.0f;
   _pitch = 0.0f;
 
+  // Mouse properties
   _mouseSensitivity = 0.1f;
   
   updateCameraVectors();
 }
 
+/**
+ * @brief Updates the camera based on the previous mouse position and current
+ * mouse position
+ * 
+ * @param dx x offset
+ * @param dy y offset
+ */
 void Camera::handleMouseEvent(float dx, float dy) {
+  // Adjust offset based on mouse sensitivity
   dx *= _mouseSensitivity;
   dy *= _mouseSensitivity;
 
+  // Update yaw and pitch
   _yaw += dx;
   _pitch += dy;
 
-  if (_pitch > 89.0f) _pitch = 89.0f;
+  // Prevent the camera from over rotating 
+  if (_pitch >  89.0f) _pitch =  89.0f;
   if (_pitch < -89.0f) _pitch = -89.0f;
 
   updateCameraVectors();
 }
 
+/**
+ * @brief Updates the front, right and up vector based on the current 
+ * values of yaw and pitch
+ * 
+ */
 void Camera::updateCameraVectors() {
+  // Update front vector 
   glm::vec3 front;
   front.x = cos(glm::radians(_yaw)) * cos(glm::radians(_pitch));
   front.y = sin(glm::radians(_pitch));
   front.z = sin(glm::radians(_yaw)) * cos(glm::radians(_pitch));
   _front = glm::normalize(front);
   
+  // Update right and up vector
   _right = glm::normalize(glm::cross(_front, _worldUp));
   _up = glm::normalize(glm::cross(_right, _front));
 }
--- camera.h
+++ camera.h
@@ -9,23 +9,43 @@ class Camera {
  public:
   Camera(glm::vec3 position);
 
+  /**
+   * @brief Returns the position vector
+   *
+   * @return glm::vec3 position
+   */
   glm::vec3 position() { return _position; }
+
+  /**
+   * @brief Returns the target vector
+   *
+   * @return glm::vec3 target
+   */
   glm::vec3 target() { return _position + _front; }
-  glm::vec3 up() {return _worldUp; }
+
+  /**
+   * @brief Returns the up vector
+   *
+   * @return glm::vec3 up
+   */
+  glm::vec3 up() { return _worldUp; }
 
   void handleMouseEvent(float dx, float dy);
 
  private:
-  glm::vec3 _position;
-  glm::vec3 _front;
-  glm::vec3 _up;
-  glm::vec3 _right;
-  glm::vec3 _worldUp;
+  // Camera properties
+  glm::vec3 _position;  // Position vector
+  glm::vec3 _front;     // Front vector
+  glm::vec3 _up;        // Up vector
+  glm::vec3 _right;     // Right vector
+  glm::vec3 _worldUp;   // World up vector
 
-  float _yaw;
-  float _pitch;
+  // Euler angles
+  float _yaw;   // Yaw
+  float _pitch; // Pitch
 
-  float _mouseSensitivity;
+  // Mouse properties
+  float _mouseSensitivity;  // Mouse sensitivity
 
   void updateCameraVectors();
 };
